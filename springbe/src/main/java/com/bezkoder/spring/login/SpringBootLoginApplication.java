ee641808fd4a44abb592fb178ce5427e924f35f8
toanphysics <nvttoan2017@gmail.com>
2023-05-18 00:02:33 +0700

    update application

--- springbe/src/main/java/com/bezkoder/spring/login/SpringBootLoginApplication.java
+++ springbe/src/main/java/com/bezkoder/spring/login/SpringBootLoginApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootLoginExampleApplication {
+public class SpringBootLoginApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootLoginExampleApplication.class, args);
+		SpringApplication.run(SpringBootLoginApplication.class, args);
 	}
 
 }
